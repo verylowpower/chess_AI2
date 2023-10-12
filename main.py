6c99a78465d08f2f10f77a69ac00e8dcfc1046e6
Ruongvit <doudqwer@gmail.com>
2023-10-12 23:24:47 +0700

    xong(thiếu castling)

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ Sq_size= Height//Dimesion #// chia lam tron xuong
 Max_fps=15 #dung de tao animation
 Asset={}
 
+        
+
 def LoadAsset():
     Pieces=['bB','bK','bN','bP','bQ','bR','wB','wK','wN','wP','wQ','wR'] #ham nay se la ten cua cac quan co trong file asset
     for piece in Pieces: #su dung vong lap de load asset vao trong ham 
@@ -26,10 +28,11 @@ def main():
     ValidMove = Gs.GetValidMove()#khi player di chuyen 1 quan co se luu vao list, sau do no se kiem tra xem nuoc di day co chieu tuong ko
     #khi player tao ra 1 nuoc di chieu tuong, doi phuong thay doi nuoc di thi ham nay se dc lam moi, tranh viec goi ham lien tuc lam game cham di
     MoveMade = False #day nhu 1 flag moi khi nuoc di dc tao ra
-    
+    Animate = False
     LoadAsset()
     Sq_selected = () #tao tuple de track lan click cuoi cung (tuple:(row,col))
     Player_click = [] #track click cua nguoi choi, vd tuple[(7,4);(4,4)] nguoi choi click vao quan co o vi tri (7,4) sau do di chuyen ra vi tri (4,4)
+    GameOver = False
     #thoat game 
     running=True
     while running:
@@ -38,53 +41,98 @@ def main():
                 running = False    
                 #track chuot
             elif e.type == pygame.MOUSEBUTTONDOWN:#ghi nhan xem chuot bam xuong(down) nhu nao, trong bao lau, trai hay phai,..
-                Location=pygame.mouse.get_pos()#toa do x, y cua chuot
-                #gan toa do x y vao chuot 
-                #lam nhu nay ta co the biet duoc chuot dang chon o vuong o vi tri nao
-                col = Location[0]//Sq_size
-                row = Location[1]//Sq_size
-                #neu nhu player bam vao o vuong 2 lan thi no co the nhan dien la da di 1 nuoc, vay nen ta can tranh truong hop do
-                if Sq_selected == (row,col): #neu player click vao 1 o vuong 2 lan
-                    Sq_selected =() #lam cho tuple rong = bo chon
-                    Player_click =[] #nhu tren
-                else: #khi nuoc di day hop le
-                    Sq_selected=(row,col) #luu 2 bien col, row vao tuple
-                    Player_click.append(Sq_selected) #them o vuong duoc chon vao danh sach quan co duoc chon
+                if not GameOver:
+                    Location=pygame.mouse.get_pos()#toa do x, y cua chuot
+                    #gan toa do x y vao chuot 
+                    #lam nhu nay ta co the biet duoc chuot dang chon o vuong o vi tri nao
+                    col = Location[0]//Sq_size
+                    row = Location[1]//Sq_size
+                    #neu nhu player bam vao o vuong 2 lan thi no co the nhan dien la da di 1 nuoc, vay nen ta can tranh truong hop do
+                    if Sq_selected == (row,col): #neu player click vao 1 o vuong 2 lan
+                        Sq_selected =() #lam cho tuple rong = bo chon
+                        Player_click =[] #nhu tren
+                    else: #khi nuoc di day hop le
+                        Sq_selected=(row,col) #luu 2 bien col, row vao tuple
+                        Player_click.append(Sq_selected) #them o vuong duoc chon vao danh sach quan co duoc chon
                     
-                if len(Player_click)==2: 
-                    move = chess_engine.Move(Player_click[0],Player_click[1],Gs.board)
-                    print(move.GetChessNotation())
-                    if move in ValidMove:
-                        Gs.MakeMove(move) #khi 1 nuoc di dung luat -> tiep tuc move
-                        MoveMade = True #True thi cho phep quan co di   
-                        Sq_selected =() #reset vung chon cua player
-                        Player_click = []
-                    else:
-                        Player_click = [Sq_selected]
+                    if len(Player_click)==2: 
+                        move = chess_engine.Move(Player_click[0],Player_click[1],Gs.board)
+                        print(move.GetChessNotation())
+                        if move in ValidMove:
+                            if not Gs.Wturn:
+                                print("White turn!")
+                            else:
+                                print("Black Turn!")                 
+                            Gs.MakeMove(move) #khi 1 nuoc di dung luat -> tiep tuc move
+                            MoveMade = True #True thi cho phep quan co di 
+                            Animate = True  
+                            Sq_selected =() #reset vung chon cua player
+                            Player_click = []
+                        else:
+                            Player_click = [Sq_selected]
+                    
             #gan chuc nang
             elif e.type == pygame.KEYDOWN: #KEYDOWN la nhan phim
                 if e.key == pygame.K_z: #gan vao phim z
                     Gs.UndoMove()
                     MoveMade = True
+                    Animate = False
                     print("undo")
+                if e.key == pygame.K_r:
+                    Gs = chess_engine.Gamestate()
+                    ValidMove = Gs.GetValidMove()
+                    Sq_selected = ()
+                    Player_click = ()
+                    MoveMade = False
+                    Animate = False
         
         if MoveMade:
+            if Animate:
+                Animation(Gs.Movelog[-1], Screen, Gs.board, Clock)
             ValidMove = Gs.GetValidMove()
             MoveMade = False 
+            Animate = False
             
         
-        drawGamestate(Screen,Gs)
+        drawGamestate(Screen,Gs,ValidMove,Sq_selected)
+        
+        if Gs.CheckMate:
+            GameOver = True
+            if Gs.Wturn:
+                drawText(Screen,'Black win!')
+            else:
+                drawText(Screen,'White win!')  
+        elif Gs.StaleMate:
+            GameOver = True
+            drawText(Screen,'Stalemate!')  
+        
         Clock.tick(Max_fps)
         pygame.display.flip()
 
+
+def highlightSq(Screen,Gs,ValidMove,Sq_selected):
+    if Sq_selected  != ():
+        r,c = Sq_selected
+        if Gs.board[r][c][0] == ("w" if Gs.Wturn else "b"):
+            s = pygame.Surface((Sq_size,Sq_size))
+            s.set_alpha(100)
+            s.fill(pygame.Color('blue'))
+            Screen.blit(s,(c*Sq_size,r*Sq_size))
+            s.fill(pygame.Color('yellow'))
+            for move in ValidMove:
+                if move.startRow == r and move.startCol == c:
+                    Screen.blit(s,(move.endCol*Sq_size,move.endRow*Sq_size))
+
 #chiu trach nhiem hien thi tat ca do hoa cua class Gamestate
-def drawGamestate(Screen,Gs):
+def drawGamestate(Screen,Gs,ValidMove,Sq_selected):
     drawBoard(Screen) #ve o vuong tren ban co
+    highlightSq(Screen,Gs,ValidMove,Sq_selected)
     drawPiece(Screen,Gs.board) #ve quan co
     
 
 #ve o vuong
 def drawBoard(Screen):
+    global Colors
     #o vuong goc tren ben trai luon la mau sang
     Colors =[pygame.Color("white"),pygame.Color("gray")] #color dau la mau sang, sau la toi
     for r in range(Dimesion):
@@ -102,9 +150,34 @@ def drawPiece(Screen,board):
             if piece != "--": #neu quan co ko phai khoang trong (tham chieu ham board trong Gamestate)
                 Screen.blit(Asset[piece],pygame.Rect(c*Sq_size,r*Sq_size,Sq_size,Sq_size))
                 
-            
+def Animation(move, Screen, board, Clock):
+    global Colors
+    dR = move.endRow - move.startRow
+    dC = move.endCol - move.startCol
+    Fps = 10
+    frameCount = (abs(dR)+abs(dC))*Fps
+    for f in range(frameCount+1):
+        r,c = (move.startRow +dR*f/frameCount, move.startCol+dC*f/frameCount)
+        drawBoard(Screen)
+        drawPiece(Screen,board)
+        
+        Color = Colors[(move.endRow + move.endCol)%2]
+        endSq = pygame.Rect(move.endCol*Sq_size,move.endRow*Sq_size,Sq_size,Sq_size)
+        pygame.draw.rect(Screen,Color,endSq)
+        if move.pieceCaptured != "--" :
+            Screen.blit(Asset[move.pieceCaptured],endSq)
 
+        Screen.blit(Asset[move.pieceMove], pygame.Rect(c*Sq_size,r*Sq_size,Sq_size,Sq_size))
+        pygame.display.flip()
+        Clock.tick(60)
 
+def drawText(Screen,text):
+    font = pygame.font.SysFont("Helvitca",32,True,False)
+    textObject = font.render(text,0, pygame.Color('White'))
+    textLocation = pygame.Rect(0,0,Width,Height).move(Width/2-textObject.get_width()/2,Height/2-textObject.get_height()/2)
+    Screen.blit(textObject,textLocation)
+    textObject = font.render(text,0,pygame.Color('Black')) 
+    Screen.blit(textObject,textLocation.move(2,2))
 
 if __name__=="__main__":
     main()
